0ce503b2352616fccbb1d2b6ebf22f2333d3edbd
Arne Gevaert <arnegevaert@gmail.com>
2017-09-28 16:22:29 +0200

    day per day

--- calc.js
+++ calc.js
@@ -1,6 +1,5 @@
 const n3 = require('n3');
 const fs = require('fs');
-const moment = require('moment');
 
 if (process.argv.length !== 4) {
     console.error('Usage: node calc.js INPUT OUTPUT');
@@ -26,9 +25,9 @@ const out_dir = process.argv[3];
 const filenames = fs.readdirSync(in_dir).sort();
 const parser = n3.Parser();
 
-const dayToGraph = {};
-const measurements = [];
-let firstDay = true;
+let dayToGraph = {};
+let measurements = [];
+let currentDay = undefined;
 filenames.forEach(file => {
     let endedDay = false;
 
@@ -45,23 +44,36 @@ filenames.forEach(file => {
     triples.forEach(triple => {
         if (triple.predicate === 'http://www.w3.org/ns/prov#generatedAtTime') {
             let day = n3.Util.getLiteralValue(triple.object).substring(0, 10);
-            if (dayToGraph[day] === undefined) {
+            if (currentDay === undefined) {
+                currentDay = day;
                 dayToGraph[day] = [];
-                if (!firstDay) {
-                    endedDay = true;
-                }
-                firstDay = false;
+            } else if (dayToGraph[day] === undefined) {
+                dayToGraph[day] = [];
+                endedDay = true;
             }
             dayToGraph[day].push(triple.subject);
-        } else if (triple.predicate === 'datex:parkingNumberOfVacantSpaces') {
+        } else if (triple.predicate === 'http://vocab.datex.org/terms#parkingNumberOfVacantSpaces') {
             measurements.push(triple);
         }
     });
 
     // We encountered the end of a day, summarize it and throw away the data
+    // Note that we assume here that at most 1 day can be ended in a file (so files are smaller than 1 day)
     if (endedDay) {
+        const graphs = dayToGraph[currentDay];
+        delete dayToGraph[currentDay];
+        currentDay = undefined;
 
+        let currentMeasurements = [];
+        let otherMeasurements = [];
+        measurements.forEach(meas => {
+            if (graphs.includes(meas.graph)) {
+                currentMeasurements.push(meas);
+            } else {
+                otherMeasurements.push(meas);
+            }
+        });
+        measurements = otherMeasurements;
+        console.log(currentMeasurements);
     }
 });
-
-console.log(JSON.stringify(dayToGraph), undefined, 2);
